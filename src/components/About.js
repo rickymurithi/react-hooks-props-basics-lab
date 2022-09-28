68b7933e5db92af624f386f77f090fd309e52255
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-09-28 09:48:18 +0300

    add file

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,20 @@
 import React from "react";
+import  Links  from "./Links"
 
-function About() {
+
+function About({bio, links}) {
+  console.log(bio, links)
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio ? <p>{bio}</p> : null};
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links bio='I made this!'
+      github = 'https://github.com/liza'
+      linkedin = 'https://www.linkedin.com/in/liza/' />
     </div>
+    
   );
 }
 
